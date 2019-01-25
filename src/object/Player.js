9ed5c908b4017c8cb05a934fe2fb558d56e7a267
PPakalns <peterispakalns@gmail.com>
2019-01-25 23:44:59 +0200

    Refactored player functions

--- src/object/Player.js
+++ src/object/Player.js
@@ -1,4 +1,44 @@
-function updatePlayer(player, cursors) {
+function DefinePlayerAnimations(scene) {
+    scene.anims.create({
+        key: 'player-still',
+        frames: [ {  key: 'characters', frame: 7 } ],
+        frameRate: 10,
+        repeat: -1,
+    })
+    scene.anims.create({
+        key: 'player-down',
+        frames: scene.anims.generateFrameNumbers('characters', {start: 6, end: 8}),
+        frameRate: 10,
+        repeat: -1,
+    })
+    scene.anims.create({
+        key: 'player-left',
+        frames: scene.anims.generateFrameNumbers('characters', {start: 18, end: 20}),
+        frameRate: 10,
+        repeat: -1,
+    })
+    scene.anims.create({
+        key: 'player-right',
+        frames: scene.anims.generateFrameNumbers('characters', {start: 30, end: 32}),
+        frameRate: 10,
+        repeat: -1,
+    })
+    scene.anims.create({
+        key: 'player-up',
+        frames: scene.anims.generateFrameNumbers('characters', {start: 42, end: 44}),
+        frameRate: 10,
+        repeat: -1,
+    })
+}
+
+function Initialize(scene) {
+    let player = scene.physics.add.sprite(0, 0, 'characters')
+    player.setCollideWorldBounds(true)
+    scene.cameras.main.startFollow(player, true, 0.1, 0.1)
+    return player;
+}
+
+function UpdatePlayer(player, cursors) {
 
     let animation_name = 'player-still'
 
@@ -23,5 +63,7 @@ function updatePlayer(player, cursors) {
 }
 
 export default {
-    updatePlayer,
+    DefinePlayerAnimations,
+    Initialize,
+    UpdatePlayer,
 }
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -16,6 +16,7 @@ export class GameScene extends Phaser.Scene {
     constructor() {
         super({key: 'gameScene'} );
     }
+
     preload() {
         this.load.image('tiles', 'assets/basictiles.png');
         this.load.spritesheet('characters', 'assets/characters.png',
@@ -24,37 +25,6 @@ export class GameScene extends Phaser.Scene {
 
     create() {
         let level = create2DArray(100, 100, 10)
-
-        this.anims.create({
-            key: 'player-still',
-            frames: [ {  key: 'characters', frame: 7 } ],
-            frameRate: 10,
-            repeat: -1,
-        })
-        this.anims.create({
-            key: 'player-down',
-            frames: this.anims.generateFrameNumbers('characters', {start: 6, end: 8}),
-            frameRate: 10,
-            repeat: -1,
-        })
-        this.anims.create({
-            key: 'player-left',
-            frames: this.anims.generateFrameNumbers('characters', {start: 18, end: 20}),
-            frameRate: 10,
-            repeat: -1,
-        })
-        this.anims.create({
-            key: 'player-right',
-            frames: this.anims.generateFrameNumbers('characters', {start: 30, end: 32}),
-            frameRate: 10,
-            repeat: -1,
-        })
-        this.anims.create({
-            key: 'player-up',
-            frames: this.anims.generateFrameNumbers('characters', {start: 42, end: 44}),
-            frameRate: 10,
-            repeat: -1,
-        })
 
         let map = this.make.tilemap({
             data: level,
@@ -67,20 +37,17 @@ export class GameScene extends Phaser.Scene {
 
         this.cameras.main.setZoom(2)
         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
-
         this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
 
-        let player = this.physics.add.sprite(0, 0, 'characters')
-        player.setCollideWorldBounds(true)
-        this.cameras.main.startFollow(player, true, 0.1, 0.1)
+        Player.DefinePlayerAnimations(this)
 
         this.e = {
-            player,
+            player: Player.Initialize(this),
             cursors: this.input.keyboard.createCursorKeys(),
         }
     }
 
     update() {
-        Player.updatePlayer(this.e.player, this.e.cursors)
+        Player.UpdatePlayer(this.e.player, this.e.cursors)
     }
 }
