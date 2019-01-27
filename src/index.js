f1c8747240bcc613e6abe98415937bd3ac7ca5f9
PPakalns <peterispakalns@gmail.com>
2019-01-27 10:02:23 +0200

    Improved fullscreen

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { GameScene } from './scenes/GameScene';
 import { MenuScene } from './scenes/MenuScene';
 import { GuiScene } from './scenes/GuiScene';
 
-const gameConfig = {
+var gameConfig = {
     width: 1080,
     height: 600,
     pixelArt: true,
@@ -14,4 +14,24 @@ const gameConfig = {
     scene: [MenuScene, GameScene, GuiScene]
 };
 
-new Phaser.Game(gameConfig);
+var game = new Phaser.Game(gameConfig);
+
+function resize() {
+  var canvas = document.querySelector("canvas");
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  var windowRatio = windowWidth / windowHeight;
+  var gameRatio = game.config.width / game.config.height;
+  console.log(windowWidth, windowHeight, windowRatio, gameRatio)
+  if (windowRatio < gameRatio) {
+    canvas.style.width = windowWidth + "px";
+    canvas.style.height = (windowWidth / gameRatio) + "px";
+  } else {
+    canvas.style.width = (windowHeight * gameRatio) + "px";
+    canvas.style.height = windowHeight + "px";
+  }
+}
+
+setTimeout(resize, 100);
+window.addEventListener("resize", resize, false);
+
